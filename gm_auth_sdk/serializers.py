c95bb13d2043bd327fc58db7b75b3bf2f8b815c2
dfg-98 <dariofg98@gmail.com>
2023-05-24 12:21:30 -0400

    feat(serializers): add Current User ID serializer

--- gm_auth_sdk/serializers.py
+++ gm_auth_sdk/serializers.py
@@ -6,6 +6,14 @@ from .models import TokenUser
 class CurrentUserDefault(RestCurrentUserDefault):
     requires_context = True
 
-    def __call__(self, serializer_field):
+    def __call__(self, serializer_field) -> TokenUser:
         user: TokenUser = serializer_field.context["request"].user
+        return user
+
+
+class CurrentUserDefaultID(CurrentUserDefault):
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        user = super().__call__(serializer_field)
         return user.id
